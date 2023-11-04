b1321f9a077d48433d1046c899e7fabd5b3bc535
duchienmtp <duchien230904@gmail.com>
2023-11-04 09:47:56 +0700

    Updating Person, App and Pupil classes

--- src/main/java/Classes/Person/Date.java
+++ src/main/java/Classes/Person/Date.java
@@ -1,5 +1,45 @@
 package Classes.Person;
 
 public class Date {
-    
+    private String date;
+    private String month;
+    private String year;
+
+    public Date() {
+    }
+
+    public Date(String date, String month, String year) {
+        this.date = date;
+        this.month = month;
+        this.year = year;
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getMonth() {
+        return this.month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getYear() {
+        return this.year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return date + "/" + month + "/" + year;
+    }
 }
